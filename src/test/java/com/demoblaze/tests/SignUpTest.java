2830b2df9d089b5f87783b84a9d7baafa707d7c7
Suparna Soman <suparnasoman@MBPl19C02D1JP1MD6M.local>
2021-12-08 13:26:56 -0500

    steps in test case added

--- src/test/java/com/demoblaze/tests/SignUpTest.java
+++ src/test/java/com/demoblaze/tests/SignUpTest.java
@@ -3,6 +3,8 @@ package com.demoblaze.tests;
 import com.demoblaze.steps.serenity.DemoblazeSteps;
 import net.thucydides.core.annotations.Managed;
 import net.thucydides.core.annotations.Steps;
+import net.thucydides.core.annotations.Title;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 
 public class SignUpTest {
@@ -13,4 +15,17 @@ public class SignUpTest {
     @Steps
     DemoblazeSteps user;
 
+    @Test
+    @Title(Add a new user to store);
+
+    String userName = "user001";
+    String password = "test123";
+
+
+    user.openStore();
+    user.navigateToSignUpLink();
+    user.addNewUserInfo(userName, password);
+    user.clickSignUp();
+
 }
+
