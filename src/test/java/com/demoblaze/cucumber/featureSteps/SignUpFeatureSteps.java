16fe67ece33d536f1657120154a811860e4a0498
Suparna Soman <suparnasoman@MBPl19C02D1JP1MD6M.localdomain>
2022-05-05 11:27:37 -0400

    TestSuite updated for user signup1

--- src/test/java/com/demoblaze/cucumber/featureSteps/SignUpFeatureSteps.java
+++ src/test/java/com/demoblaze/cucumber/featureSteps/SignUpFeatureSteps.java
@@ -1,48 +1,48 @@
-//package com.demoblaze.cucumber.featureSteps;
-//
-//import com.demoblaze.cucumber.steps.BaseSteps;
-//import com.demoblaze.cucumber.steps.SignUpSteps;
-//import io.cucumber.java.en.And;
-//import io.cucumber.java.en.Given;
-//import io.cucumber.java.en.Then;
-//import io.cucumber.java.en.When;
-//import net.thucydides.core.annotations.Steps;
-//
-//public class SignUpFeatureSteps {
-//
-//    @Steps
-//    SignUpSteps signupUser;
-//
-//    @Steps
-//    BaseSteps baseUser;
-//
-//    @Given("user is at DemoBlaze home page")
-//    public void userIsAtDemoBlazeLandingPage() {
-//        baseUser.navigateToHomePage();
-//    }
-//
-//    @And("user clicks on the Sign up link")
-//    public void userClicksOnTheSignupLink() {
-//        signupUser.navigateToSignUp();
-//    }
-//
-//    @When("user enters the {word} and {word}")
-//    public void userAddUserNameAndPassword(String userName, String password) {
-//        signupUser.addNewUserInfo(userName, password);
-//    }
-//
-//    @And("user clicks on Sign up button")
-//    public void userClicksOnSignUp() {
-//        signupUser.signUp();
-//    }
-//
-//    @Then("an alert message should say the user already exist")
-//    public void userViewTheAlertMessageSayingTheUserExist() {
-//        signupUser.verifyIfUserExist();
-//    }
-//
-//    @Then("an alert message should say Sign up is successful")
-//    public void userViewTheAlertMessageSayingSignUpIsSuccessful() {
-//        signupUser.verifySuccessfulSignup();
-//    }
-//}
+package com.demoblaze.cucumber.featureSteps;
+
+import com.demoblaze.cucumber.steps.BaseSteps;
+import com.demoblaze.cucumber.steps.SignUpSteps;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import net.thucydides.core.annotations.Steps;
+
+public class SignUpFeatureSteps {
+
+    @Steps
+    SignUpSteps signupUser;
+
+    @Steps
+    BaseSteps baseUser;
+
+    @Given("user is at DemoBlaze home page")
+    public void userIsAtDemoBlazeLandingPage() {
+        baseUser.navigateToHomePage();
+    }
+
+    @And("user clicks on the Sign up link")
+    public void userClicksOnTheSignupLink() {
+        signupUser.navigateToSignUp();
+    }
+
+    @When("user enters the {word} and {word}")
+    public void userAddUserNameAndPassword(String userName, String password) {
+        signupUser.addNewUserInfo(userName, password);
+    }
+
+    @And("user clicks on Sign up button")
+    public void userClicksOnSignUp() {
+        signupUser.signUp();
+    }
+
+    @Then("an alert message should say the user already exist")
+    public void userViewTheAlertMessageSayingTheUserExist() {
+        signupUser.verifyIfUserExist();
+    }
+
+    @Then("an alert message should say Sign up is successful")
+    public void userViewTheAlertMessageSayingSignUpIsSuccessful() {
+        signupUser.verifySuccessfulSignup();
+    }
+}
--- src/test/resources/features/user-signup.feature
+++ src/test/resources/features/user-signup.feature
@@ -1,25 +1,25 @@
-#Feature: Sign up to the DemoBlaze store
-#
-#  @test1
-#  Scenario Outline: Sign up as a new user to the Demo Blaze store but the user already exists
-#    Given user is at DemoBlaze home page
-#    And user clicks on the Sign up link
-#    When user enters the <userName> and <password>
-#    And user clicks on Sign up button
-#    Then an alert message should say the user already exist
-#    Examples:
-#      | userName | password |
-#      | user001  | test123  |
-#      | user002  | test123  |
-#
-#  @test2
-#  Scenario Outline: Sign up as a new user to the Demo Blaze store and user doesn't exist
-#    Given user is at DemoBlaze home page
-#    And user clicks on the Sign up link
-#    When user enters the <userName> and <password>
-#    And user clicks on Sign up button
-#    Then an alert message should say Sign up is successful
-#    Examples:
-#      | userName   | password |
-#      | newuser_23 | test123  |
-#      | newuser_24 | test123  |
+Feature: Sign up to the DemoBlaze store
+
+  @test1
+  Scenario Outline: Sign up as a new user to the Demo Blaze store but the user already exists
+    Given user is at DemoBlaze home page
+    And user clicks on the Sign up link
+    When user enters the <userName> and <password>
+    And user clicks on Sign up button
+    Then an alert message should say the user already exist
+    Examples:
+      | userName | password |
+      | user001  | test123  |
+      | user002  | test123  |
+
+  @test2
+  Scenario Outline: Sign up as a new user to the Demo Blaze store and user doesn't exist
+    Given user is at DemoBlaze home page
+    And user clicks on the Sign up link
+    When user enters the <userName> and <password>
+    And user clicks on Sign up button
+    Then an alert message should say Sign up is successful
+    Examples:
+      | userName   | password |
+      | newuser_23 | test123  |
+      | newuser_24 | test123  |
